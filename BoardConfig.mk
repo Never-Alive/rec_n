289451f13890959ff3556b9679d4f018a23c6ae0
Never-Alive <livinglucifer0@gmail.com>
2025-12-26 12:30:41 +0530

    haydn: Bringup for OrangeFox!
    Signed-off-by: Never-Alive <livinglucifer0@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -177,8 +177,5 @@ TW_BATTERY_PATH := /sys/class/power_supply/battery
 TW_BACKUP_EXCLUSIONS := /data/fonts,/data/adb/ap,/data/adb/ksu
 TW_LOAD_VENDOR_MODULES := "msm_drm.ko focaltech_touch.ko adsp_loader_dlkm.ko qti_battery_charger_main.ko exfat.ko"
 
-# Maintainer/Version
-TW_DEVICE_VERSION := by_Lucifer
-
 # FPS
  TW_FRAMERATE := 120
