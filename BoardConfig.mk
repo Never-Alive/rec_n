c74db0b6b57b475346ed008ca66d0d40afc4fc00
Never-Alive <livinglucifer0@gmail.com>
2026-02-24 22:13:52 +0530

    haydn: Remove all modules Our kernel is inlined due to that eveything is baked into "kernel/Image"
    Signed-off-by: Never-Alive <livinglucifer0@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -66,9 +66,6 @@ BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
 # DTBO
 BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilts/dtbo.img
 
-# Kernel Modules
-NEED_KERNEL_MODULE_RECOVERY := true
-
 # A/B
 BOARD_USES_RECOVERY_AS_BOOT := true
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := false
@@ -172,7 +169,6 @@ TW_SUPPORT_INPUT_AIDL_HAPTICS_FIX_OFF := true
 TW_CUSTOM_CPU_TEMP_PATH := "/sys/class/thermal/thermal_zone20/temp"
 TW_BATTERY_PATH := /sys/class/power_supply/battery
 TW_BACKUP_EXCLUSIONS := /data/fonts,/data/adb/ap,/data/adb/ksu
-TW_LOAD_VENDOR_MODULES := "msm_drm.ko focaltech_touch.ko adsp_loader_dlkm.ko qti_battery_charger_main.ko exfat.ko"
 
 # FPS
  TW_FRAMERATE := 60
